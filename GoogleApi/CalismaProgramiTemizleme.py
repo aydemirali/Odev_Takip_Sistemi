b6be0c9bb979ca2ae18aebfbb0810dd17c3dd2c0
aydemirali <aydemirali@gmail.com>
2019-01-13 19:46:53 +0300

    Add files via upload

---
+++ GoogleApi/CalismaProgramiTemizleme.py
@@ -0,0 +1,214 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('OdevSistemi-fc12779d1a3c.json', scope)
+client = gspread.authorize(creds)
+
+def besinciSinif(tarih):
+
+    sheet = client.open('5. Sınıflar Haftalık Çalışma Programı').sheet1
+
+    sheet.update_acell('H2', tarih)
+    sheet.update_acell('B6','')
+    sheet.update_acell('C6', '')
+    sheet.update_acell('D6', '')
+    sheet.update_acell('E6', '')
+    sheet.update_acell('F6', '')
+    sheet.update_acell('G6', '')
+    sheet.update_acell('H6', '')
+    sheet.update_acell('B9', '')
+    sheet.update_acell('C9', '')
+    sheet.update_acell('D9', '')
+    sheet.update_acell('E9', '')
+    sheet.update_acell('F9', '')
+    sheet.update_acell('G9', '')
+    sheet.update_acell('H9', '')
+    sheet.update_acell('B12', '')
+    sheet.update_acell('C12', '')
+    sheet.update_acell('D12', '')
+    sheet.update_acell('E12', '')
+    sheet.update_acell('F12', '')
+    sheet.update_acell('G12', '')
+
+    print("5. Sınıf Çalışma Programı Sıfırlanmıştır")
+
+def altinciSinif(tarih):
+
+    sheet = client.open('6. Sınıflar Haftalık Çalışma Programı').sheet1
+
+    sheet.update_acell('H2', tarih)
+    sheet.update_acell('B6','')
+    sheet.update_acell('C6', '')
+    sheet.update_acell('D6', '')
+    sheet.update_acell('E6', '')
+    sheet.update_acell('F6', '')
+    sheet.update_acell('G6', '')
+    #sheet.update_acell('H6', '')
+    sheet.update_acell('B9', '')
+    sheet.update_acell('C9', '')
+    sheet.update_acell('D9', '')
+    sheet.update_acell('E9', '')
+    sheet.update_acell('F9', '')
+    sheet.update_acell('G9', '')
+    #sheet.update_acell('H9', '')
+    sheet.update_acell('B12', '')
+    sheet.update_acell('C12', '')
+    sheet.update_acell('D12', '')
+    sheet.update_acell('E12', '')
+    sheet.update_acell('F12', '')
+    sheet.update_acell('G12', '')
+
+    print("6. Sınıf Çalışma Programı Sıfırlanmıştır")
+
+def yedinciSinif(tarih):
+
+    sheet = client.open('7. Sınıflar Haftalık Çalışma Programı').sheet1
+
+    sheet.update_acell('H2', tarih)
+    sheet.update_acell('B6','')
+    sheet.update_acell('C6', '')
+    sheet.update_acell('D6', '')
+    sheet.update_acell('E6', '')
+    sheet.update_acell('F6', '')
+    sheet.update_acell('G6', '')
+    sheet.update_acell('H6', '')
+    sheet.update_acell('B9', '')
+    sheet.update_acell('C9', '')
+    sheet.update_acell('D9', '')
+    sheet.update_acell('E9', '')
+    sheet.update_acell('F9', '')
+    sheet.update_acell('G9', '')
+    sheet.update_acell('H9', '')
+    sheet.update_acell('B12', '')
+    sheet.update_acell('C12', '')
+    sheet.update_acell('D12', '')
+    sheet.update_acell('E12', '')
+    sheet.update_acell('F12', '')
+    sheet.update_acell('G12', '')
+
+    print("7. Sınıf Çalışma Programı Sıfırlanmıştır")
+
+def sekizASinifi(tarih):
+
+    sheet = client.open('8-A Sınıfı Haftalık Çalışma Programı').sheet1
+
+    sheet.update_acell('H2', tarih)
+    sheet.update_acell('B6','')
+    sheet.update_acell('C6', '')
+    sheet.update_acell('D6', '')
+    sheet.update_acell('E6', '')
+    sheet.update_acell('F6', '')
+    sheet.update_acell('G6', '')
+    sheet.update_acell('H6', '')
+    sheet.update_acell('B9', '')
+    sheet.update_acell('C9', '')
+    sheet.update_acell('D9', '')
+    sheet.update_acell('E9', '')
+    sheet.update_acell('F9', '')
+    sheet.update_acell('G9', '')
+    sheet.update_acell('H9', '')
+    sheet.update_acell('B12', '')
+    sheet.update_acell('C12', '')
+    sheet.update_acell('D12', '')
+    sheet.update_acell('E12', '')
+    sheet.update_acell('F12', '')
+    sheet.update_acell('G12', '')
+    sheet.update_acell('H12', '')
+    sheet.update_acell('B15', '')
+    sheet.update_acell('C15', '')
+    sheet.update_acell('D15', '')
+    sheet.update_acell('E15', '')
+    sheet.update_acell('F15', '')
+    sheet.update_acell('G15', '')
+    sheet.update_acell('H15', '')
+    sheet.update_acell('H18', '')
+
+    print("8-A Sınıfı Çalışma Programı Sıfırlanmıştır")
+
+def sekizBSinifi(tarih):
+
+    sheet = client.open('8-B Sınıfı Haftalık Çalışma Programı').sheet1
+
+    sheet.update_acell('H2', tarih)
+    sheet.update_acell('B6', '')
+    sheet.update_acell('C6', '')
+    sheet.update_acell('D6', '')
+    sheet.update_acell('E6', '')
+    sheet.update_acell('F6', '')
+    sheet.update_acell('G6', '')
+    sheet.update_acell('H6', '')
+    sheet.update_acell('B9', '')
+    sheet.update_acell('C9', '')
+    sheet.update_acell('D9', '')
+    sheet.update_acell('E9', '')
+    sheet.update_acell('F9', '')
+    sheet.update_acell('G9', '')
+    sheet.update_acell('H9', '')
+    sheet.update_acell('B12', '')
+    sheet.update_acell('C12', '')
+    sheet.update_acell('D12', '')
+    sheet.update_acell('E12', '')
+    sheet.update_acell('F12', '')
+    sheet.update_acell('G12', '')
+    sheet.update_acell('H12', '')
+    sheet.update_acell('B15', '')
+    sheet.update_acell('C15', '')
+    sheet.update_acell('D15', '')
+    sheet.update_acell('E15', '')
+    sheet.update_acell('F15', '')
+    sheet.update_acell('G15', '')
+    sheet.update_acell('H15', '')
+    sheet.update_acell('H18', '')
+
+    print("8-B Sınıfı Çalışma Programı Sıfırlanmıştır")
+
+def sekizCSinifi(tarih):
+
+    sheet = client.open('8-C Sınıfı Haftalık Çalışma Programı').sheet1
+
+    sheet.update_acell('H2', tarih)
+    sheet.update_acell('B6', '')
+    sheet.update_acell('C6', '')
+    sheet.update_acell('D6', '')
+    sheet.update_acell('E6', '')
+    sheet.update_acell('F6', '')
+    sheet.update_acell('G6', '')
+    sheet.update_acell('H6', '')
+    sheet.update_acell('B9', '')
+    sheet.update_acell('C9', '')
+    sheet.update_acell('D9', '')
+    sheet.update_acell('E9', '')
+    sheet.update_acell('F9', '')
+    sheet.update_acell('G9', '')
+    sheet.update_acell('H9', '')
+    sheet.update_acell('B12', '')
+    sheet.update_acell('C12', '')
+    sheet.update_acell('D12', '')
+    sheet.update_acell('E12', '')
+    sheet.update_acell('F12', '')
+    sheet.update_acell('G12', '')
+    sheet.update_acell('H12', '')
+    sheet.update_acell('B15', '')
+    sheet.update_acell('C15', '')
+    sheet.update_acell('D15', '')
+    sheet.update_acell('E15', '')
+    sheet.update_acell('F15', '')
+    sheet.update_acell('G15', '')
+    sheet.update_acell('H15', '')
+    sheet.update_acell('H18', '')
+
+    print("8-C Sınıfı Çalışma Programı Sıfırlanmıştır")
+
+def sifirla():
+    tarih=input("Ödev Tarihini Giriniz: \n")
+    besinciSinif(tarih)
+    altinciSinif(tarih)
+    yedinciSinif(tarih)
+    sekizASinifi(tarih)
+    sekizBSinifi(tarih)
+    sekizCSinifi(tarih)
+
+    print("Tüm çalışma programı güncellemeleri bitmiştir. Lütfen kontol etmeyi ihmal etmeyiniz")
+
+
---
+++ GoogleApi/denemeModul.py
@@ -0,0 +1,31 @@
+import CalismaProgramiTemizleme
+import time
+
+print("""
+*****************************************
+|                                       |
+|    Ödev Programı 1.0'a Hoş geldiniz   |
+|            Ortaokul                   |
+|                                       |
+*****************************************
+""")
+
+while True:
+    print("""
+    1-Ortaokul Çalışma Programlarını Sıfırlayın
+    2-Ortaokul Raporları Sıfırlayın
+    3-Çıkış
+    """)
+    secenek=input("Lütfen Seçeneğinizi Giriniz:\n")
+    if(secenek=="3"):
+        break
+    elif(secenek=="1"):
+        print("Çalışma Programları Sıfırlanıyor....")
+        CalismaProgramiTemizleme.sifirla()
+        print("Çalışma Programları Sıfırlandı")
+    elif(secenek=="2"):
+        print("Raporlar Sıfırlanıyor....")
+        time.sleep(3)
+        print("Raporlar Sıfırlandı")
+    else:
+        print("Yanlış Bir Seçenek Seçtiniz.")
